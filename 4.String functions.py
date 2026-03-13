fa75cc45959519b65dae036c6e81cd0cd7397b64
Vikas-HL <vikas58.hl@gmail.com>
2026-03-13 15:49:17 +0530

    Completed Chapter 3: Strings

---
+++ 4.String functions.py
@@ -0,0 +1,16 @@
+name = "harry"
+
+print(len(name))
+
+print(name.endswith("ry"))
+
+print(name.endswith("rya"))
+
+print(name.startswith("ha"))
+
+print(name.startswith("sha"))
+
+print(name.capitalize())
+
+name= "Vikas is a good good guy"
+print(name.replace("good" , "bad"))
---
+++ 5.Escape sequence.py
@@ -0,0 +1,9 @@
+a = 'Harry is a good boy\nbut not a bad \'boy\''
+
+print(a)
+
+b= " Vikas is a talented\t guy"
+print(b)
+
+c = 'Harry is a good boy\nbut not a bad boy'
+print(c)
