7f4d60f38a788e966d8ac572d0bef41c99da75d5
Vikas-HL <vikas58.hl@gmail.com>
2026-03-14 23:24:41 +0530

    some practice set

---
+++ Chapter1 ps/Problem1.py
@@ -0,0 +1,16 @@
+# print('''hhsdkjfhkjsd
+# ajbhjbkjabjhdfjkna
+# asjbuashisjndkjbsauhdioasjd
+# snchgasdgbwfkjhwb
+# djhgasjkdhaskjhkjhkfhjh
+# gfgweiuyuriewhfkjyugjf
+# fjhsgjfhkjsahfjkhjkhfkf
+# skjfksdjhfkjhksnfkjhdskjdf''')
+print('''Johnny Johnny
+Yes pa pa
+Eating sugar
+No papa
+telling lies
+no papa
+oper your mouth
+ha ha ha''')
---
+++ Chapter1 ps/Problem5.py
@@ -0,0 +1,6 @@
+PIN = input("Enter PIN:")
+if PIN == "1234":
+   print("1.Withdraw 2.Deposit 3.Balance 4.Exit")
+   print("Thankyou for using the ATM!")
+else:
+    print("Invalid PIN")
