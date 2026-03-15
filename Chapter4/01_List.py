8ab545f7d2fdaf07c8b9d863108092ca70bd63fd
Vikas-HL <vikas58.hl@gmail.com>
2026-03-15 18:51:38 +0530

    Learned about List and its methods

---
+++ Chapter4/01_List.py
@@ -0,0 +1,14 @@
+friends = ["Apple","Orange",5,0.667,False,"Akash", "Rohan"]
+
+print(friends[1])
+
+friends[2] = "Grapes"#Unlike strings lists areare mjuteable
+
+print(friends[2])
+
+friends[5] = "Vikky"
+
+print(friends[5])
+print(friends[3])
+
+print(friends[1:5])
---
+++ Chapter4/02_List methods.py
@@ -0,0 +1,26 @@
+friends = ["Apple","Orange",5,0.667,False,"Akash", "Rohan"]
+
+print(friends)
+
+friends.append("Vikky")
+
+print(friends)
+l1 = [1,34,67,45,676,4,33,32]
+l1.sort()
+print(l1)
+
+l1 = [1,34,67,45,676,4,33,32]
+l1.reverse()
+print(l1)
+
+l1 = [1,34,67,45,676,4,33,32]
+l1.insert(4, 4444) #Insert 4444 at 4th index
+print(l1)
+
+l1 = [1,34,67,45,676,4,33,32]
+print(l1.pop(3)) #45 poped out
+print(l1)
+
+l1 = [1,34,67,45,676,4,33,32]
+l1.remove(34)
+print(l1)
