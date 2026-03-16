9dc964b81c5188bcc1699a3ebaed3fcecd39a6b1
Vikas-HL <vikas58.hl@gmail.com>
2026-03-16 23:08:07 +0530

    Completed chapter 4!

---
+++ Chapter4/04_Tuple methods.py
@@ -0,0 +1,19 @@
+a =(1,5456,64,4323,64,False,"Rocky") 
+print(a)
+
+no = a.count(45)
+print(no)
+
+no = a.count(5456)
+print(no)
+
+no = a.count(64)
+print(no)
+
+i = a.index(64)
+print(i)
+
+i = a.index(False)
+print(i)
+
+print(len(a))
