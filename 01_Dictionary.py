04f4c1d197705e7d2b087b22e64a9b338091f239
Vikas-HL <vikas58.hl@gmail.com>
2026-03-18 22:46:23 +0530

    Learned about Dictionary!!

---
+++ 01_Dictionary.py
@@ -0,0 +1,18 @@
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Vikas": 23,
+    0: "Harry"
+}
+
+print(marks, type(marks))
+
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23,
+    0: "Harry"
+}
+
+print(marks["Harry"])
+print(marks["Rohan"])
---
+++ 02_Dic methods.py
@@ -0,0 +1,26 @@
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23,
+    0: "Harry"
+}
+
+print(marks.items())
+
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23,
+    0: "Harry"
+}
+
+print(marks.keys())
+
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23,
+    0: "Harry"
+}
+
+print(marks.values())
