f77187e88acfc718ccaf3b101237fee26cdeb2b7
Vikas-HL <vikas58.hl@gmail.com>
2026-03-24 21:21:31 +0530

    Revision about sets

---
+++ 03_Sets.py
@@ -0,0 +1,5 @@
+s = {1,5, 5, 7}
+
+e = set()  #Dont use s = {} as it will create an empty dictionary
+
+print(s)
---
+++ 05_set union intersection.py
@@ -0,0 +1,11 @@
+s1 = {1, 45, 6}
+s2 = {7, 8, 1, 78}
+
+print(s1.union(s2))
+
+print(s1.intersection(s2))
+
+s3 = {1, 45, 6, 78}
+s4 = {7, 8, 1, 78}
+
+print(s3.intersection(s4))
